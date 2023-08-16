644204a88f3ce175ccf78aae6787fe631c24e6c9
fouedayedi <fouedayedi29@gmail.com>
2023-08-16 16:34:29 +0100

    Project intialiasation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a mobile app that allows customers to view furniture and home decor items in their spaces with a few',
+    author='fouedayedi',
+    license='MIT',
+)
